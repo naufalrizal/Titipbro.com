c204f07f03a6ab72522b5182c882c1f8395c834b
naufalrizal <naufal.076@gmail.com>
2016-05-10 14:59:28 +0700

    Update Login Page

--- layout/footer.php
+++ layout/footer.php
@@ -1,2 +1,31 @@
 </body>
+
+
+<footer>
+    <div class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3  col-md-3 col-sm-6 col-xs-12 ">
+                    <h3> Find us at</h3>
+                    <ul class="social">
+                        <li> <a href="#"> <i class=" fa fa-facebook">   </i> </a> </li>
+                        <li> <a href="#"> <i class="fa fa-twitter">   </i> </a> </li>
+                        <li> <a href="#"> <i class="fa fa-google-plus">   </i> </a> </li>
+                    </ul>
+                </div>
+            </div>
+            <!--/.row--> 
+        </div>
+        <!--/.container--> 
+    </div>
+    <!--/.footer-->
+    
+    <div class="footer-bottom">
+        <div class="container">
+            <p class="pull-left"> Copyright © Bloom. All right reserved. </p>
+        </div>
+    </div>
+    <!--/.footer-bottom--> 
+</footer>
+
 </html>
--- layout/header.php
+++ layout/header.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <title><?php if(isset($title)){ echo $title; }?></title>
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style/main.css">
- 
+    <link rel="stylesheet" href="css/dev.css">
 
-  
 </head>
 <body>
